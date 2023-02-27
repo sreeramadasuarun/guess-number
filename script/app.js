cc8088e9680e8483b2bb362975adb805ca786854
sreeramadasuarun <sreeramadasuarun@gmail.com>
2023-02-27 17:24:48 +0530

    css and functions colors implement

--- script/app.js
+++ script/app.js
@@ -6,18 +6,22 @@ document.getElementById("submitguess").onclick = function () {
   const setuserguess = document.getElementById("userguess").value;
   // .....end....
   if (setuserguess == randomNumber) {
+    output.style.backgroundColor = "#274690";
     output.textContent = "CONGRATS!!! YOU GUESS IS RIGHT";
   } else if (setuserguess > randomNumber) {
-    output.style.backgroundColor = "rgb(66,75,98)";
+    output.style.backgroundColor = "#9E829C";
     output.textContent = "TRY A SMALL NUMBER";
   } else {
     output.textContent = "TRY A HIGH NUMBER";
-    output.style.backgroundColor = "rgb(166,75,98)";
+    output.style.backgroundColor = "#FFBC0A";
   }
 };
 //.........answer
 const hint = () => {
-  return (output.textContent = `The answer is: ${randomNumber}`);
+  return (
+    (output.textContent = `The answer is: ${randomNumber}`),
+    (output.style.backgroundColor = "var(--cr--red)")
+  );
 };
 
 // document.getElementById("output").style.backgroundColor = "rgb(55,25,98)";
