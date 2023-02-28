346e863e34fc3a6aba9db52c1857605174c70854
sreeramadasuarun <sreeramadasuarun@gmail.com>
2023-02-28 20:52:31 +0530

    guess and highscore added

--- script/app.js
+++ script/app.js
@@ -1,32 +1,59 @@
+"use strict";
+
 //..........random value generated
-const randomNumber = Math.floor(Math.random() * 10) + 1;
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+const score = document.getElementById("totalscore");
+
+let setguess = 5;
+let setscore = 0;
+
+guess.textContent = setguess;
 
 //..........arrow function with onclick
-
 const submitguess = () => {
-  //...... number by user....
-  const setuserguess = Number(document.getElementById("userguess").value);
-  // .....end....
-  if (setuserguess === randomNumber) {
-    output.style.backgroundColor = "#274690";
-    output.textContent = "CONGRATS!!! YOU GUESS IS RIGHT";
-  } else if (setuserguess == "") {
-    window.alert("TYPE A NUMBER 1-10 0NLY");
-  } else if (setuserguess > 10) {
+  //...... user input....
+  const setuserguess = Number(
+    document.getElementById("presentuserguess").value
+  );
+
+  console.log(setguess);
+  if (setguess <= 0) {
+    output.textContent = " GAME OVER: " + setguess + " GUESS ";
+    gamereset.style.display = "block";
     output.style.backgroundColor = "var(--cr--red)";
-    output.textContent = "NOT TO EXCEED 10";
-  } else if (setuserguess < 1) {
-    output.style.backgroundColor = "var(--cr--red)";
-    output.textContent = "NOT TO EXCEED 10";
-  } else if (setuserguess > randomNumber) {
-    output.style.backgroundColor = "#9E829C";
-    output.textContent = "TRY A SMALL NUMBER";
   } else {
-    output.textContent = "TRY A HIGH NUMBER";
-    output.style.backgroundColor = "#FE829c";
+    if (setuserguess === randomNumber) {
+      output.style.backgroundColor = "#274690";
+
+      gamereset.style.display = "block";
+      output.textContent =
+        "CONGRATULATIONS!!! YOU GUESSED IT RIGHT  " + setguess + " GUESS ";
+      //.............pending work on score ++
+      setscore.innerHTML = setguess;
+      //..............
+      this.usersubmitguess.style.display = "none";
+    } else if (setuserguess == "") {
+      window.alert("TYPE A NUMBER 1-10 0NLY");
+    } else if (setuserguess > 10) {
+      output.style.backgroundColor = "var(--cr--red)";
+      output.textContent = "NOT TO EXCEED 10";
+    } else if (setuserguess > randomNumber) {
+      output.style.backgroundColor = "#9E829C";
+      output.textContent = "TRY A SMALL NUMBER";
+      setguess--;
+    } else {
+      output.textContent = "TRY A HIGH NUMBER";
+      output.style.backgroundColor = "#FE829c";
+      setguess--;
+    }
   }
+
+  this.presentuserguess.value = "";
+  console.log(setguess);
+  guess.textContent = setguess;
 };
-//.........answer
+
+//.........Give up button
 const hint = () => {
   return (
     (output.textContent = `The answer is: ${randomNumber}`),
@@ -34,5 +61,17 @@ const hint = () => {
   );
 };
 
-// document.getElementById("output").style.backgroundColor = "rgb(55,25,98)";
-console.log(randomNumber);
+//answer
+console.log("Answer is: " + randomNumber);
+
+//.....reset
+const Reset = () => {
+  setguess = "5";
+  guess.textContent = setguess;
+  gamereset.style.display = "none";
+  usersubmitguess.style.display = "block";
+  randomNumber = Math.floor(Math.random() * 10) + 1;
+  console.log(randomNumber);
+  output.textContent = "";
+  output.style.backgroundColor = "";
+};
